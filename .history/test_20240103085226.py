ead214879e51df06d14d0ae7fdf6064d2beb39e7
jschaon@njau.edu.cn <jschaon@njau.edu.cn>
2024-01-03 09:18:06 +0800

    update

--- .history/test_20240103085226.py
+++
@@ -1,8 +0,0 @@
-from network.phenonet import PhenoNet
-
-def main():
-    model = PhenoNet()
-    print(model.print_architecture())
-
-if __name__ == "__main__":
-    main()
--- .history/test_20240103085520.py
+++
@@ -1,8 +0,0 @@
-from arch.phenonet import PhenoNet
-
-def main():
-    model = PhenoNet()
-    model.print_architecture()
-
-if __name__ == "__main__":
-    main()
